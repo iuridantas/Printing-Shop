4925b3a0754d7e02d0b2d306aafec7f73c0e5001
iuridantas <uri.dantas@hotmail.com>
2023-10-03 22:33:11 -0300

    style: removed loading

--- src/Page/home/index.tsx
+++ src/Page/home/index.tsx
@@ -1,72 +1,45 @@
-import { useState, useEffect } from 'react';
 import { SessionAboutUs } from '../../components/session/sessionAboutUs';
 import { SessionContact } from '../../components/session/sessionContact';
 import { SessionInitial } from '../../components/session/sessionInitial';
 import { SessionProductAndService } from '../../components/session/sessionProductAndService';
 import { SessionTimes } from '../../components/session/sessionTimes';
-import { Container, Buttons, LoaderSection, Slider } from './style';
+import { Container, Buttons } from './style';
 import { FaWhatsappSquare, FaPhoneAlt } from 'react-icons/fa';
 
 export function Home() {
-  const [isLoading, setIsLoading] = useState(true);
   const phoneNumber = '077988621654';
 
   const handlePhoneButtonClick = () => {
     window.location.href = `tel:${phoneNumber}`;
   };
 
-  useEffect(() => {
-    const isContentLoaded = sessionStorage.getItem('isContentLoaded');
-
-    if (isContentLoaded) {
-      setIsLoading(false);
-    } else {
-      setTimeout(() => {
-        setIsLoading(false);
-        sessionStorage.setItem('isContentLoaded', 'true');
-      }, 2000);
-    }
-  }, []);
-
   return (
     <Container aria-label="Página inicial">
-      {isLoading ? (
-        <LoaderSection>
-          <Slider i={0} />
-          <Slider i={1} />
-          <Slider i={2} />
-          <Slider i={3} />
-          <Slider i={4} />
-        </LoaderSection>
-      ) : (
-        <>
-          <Buttons>
-            <a
-              href="https://api.whatsapp.com/send?phone=5577988621654&text=Ol%C3%A1%20JPrint%20Digital,%20vim%20atrav%C3%A9s%20do%20seu%20site,%20gostaria%20de%20obter%20mais%20informa%C3%A7%C3%B5es"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Número do WhatsApp de JPrint Digital"
-              title="WhatsApp de JPrint Digital"
-            >
-              <FaWhatsappSquare
-                className="whatsApp"
-                alt="Ícone representando o WhatsApp de JPrint Digital"
-              />
-            </a>
-            <a className="phoneButton" onClick={handlePhoneButtonClick}>
-              <FaPhoneAlt
-                className="phone"
-                alt="Ícone representando o telefone de JPrint Digital"
-              />
-            </a>
-          </Buttons>
-          <SessionInitial />
-          <SessionAboutUs />
-          <SessionProductAndService />
-          <SessionTimes />
-          <SessionContact />
-        </>
-      )}
+      <Buttons>
+        <a
+          href="https://api.whatsapp.com/send?phone=5577988621654&text=Ol%C3%A1%20JPrint%20Digital,%20vim%20atrav%C3%A9s%20do%20seu%20site,%20gostaria%20de%20obter%20mais%20informa%C3%A7%C3%B5es"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Número do WhatsApp de JPrint Digital"
+          title="WhatsApp de JPrint Digital"
+        >
+          <FaWhatsappSquare
+            className="whatsApp"
+            alt="Ícone representando o WhatsApp de JPrint Digital"
+          />
+        </a>
+        <a className="phoneButton" onClick={handlePhoneButtonClick}>
+          <FaPhoneAlt
+            className="phone"
+            alt="Ícone representando o telefone de JPrint Digital"
+          />
+        </a>
+      </Buttons>
+      <SessionInitial />
+      <SessionAboutUs />
+      <SessionProductAndService />
+      <SessionTimes />
+      <SessionContact />
     </Container>
   );
 }
--- src/Page/home/style.ts
+++ src/Page/home/style.ts
@@ -1,12 +1,11 @@
-import styled, { keyframes } from 'styled-components';
-import { ImWhatsapp } from 'react-icons/im';
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
 
   @media only screen and (max-width: 600px) {
-    padding-bottom: 8px;
+    padding-bottom: 10px;
   }
 
   @media only screen and (min-width: 768px) and (max-width: 1024px) {
@@ -28,73 +27,6 @@ export const WhatsAppButton = styled.a`
   @media only screen and (min-width: 768px) and (max-width: 1024px) {
     top: 40%;
     left: 93%;
-  }
-`;
-
-export const WhatsApp = styled(ImWhatsapp)`
-  font-size: 50px;
-  border-radius: 30px;
-  padding: 3px;
-  color: white;
-  background-color: green;
-
-  @media only screen and (max-width: 600px) {
-    font-size: 35px;
-  }
-`;
-
-export const LoaderSection = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-  margin: 15% 0;
-
-  @media only screen and (max-width: 600px) {
-    margin: 50% 0;
-  }
-`;
-
-export const animate_2 = keyframes`
-  0% {
-    transform: translateY(250px);
-    filter: hue-rotate(0deg);
-  }
-
-  50% {
-    transform: translateY(0);
-  }
-
-  100% {
-    transform: translateY(250px);
-    filter: hue-rotate(180deg);
-  }
-`;
-
-export const Slider = styled.div<{ i: number }>`
-  overflow: hidden;
-  background-color: white;
-  margin: 0 15px;
-  height: 80px;
-  width: 20px;
-  border-radius: 30px;
-  box-shadow: 15px 15px 20px rgba(0, 0, 0, 0.1), -15px -15px 30px #fff,
-    inset -5px -5px 10px rgba(0, 0, 255, 0.1),
-    inset 5px 5px 10px rgba(0, 0, 0, 0.1);
-  position: relative;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 20px;
-    width: 20px;
-    border-radius: 100%;
-    box-shadow: inset 0px 0px 0px rgba(0, 0, 0, 0.3), 0px 420px 0 400px #2697f3,
-      inset 0px 0px 0px rgba(0, 0, 0, 0.1);
-    animation: ${animate_2} 2.5s ease-in-out infinite;
-    animation-delay: calc(-0.5s * ${(props) => props.i});
   }
 `;
 
